24f4b17420060a6105c5d4e5969da6c0bfbd64ef
uronep <uronep@gmail.com>
2012-01-16 21:33:46 +0900

    first commit

--- GANAAN_CUSTOMER_MGR/MainWindow.xaml.cs
+++ GANAAN_CUSTOMER_MGR/MainWindow.xaml.cs
@@ -22,6 +22,25 @@ namespace GANAAN_CUSTOMER_MGR
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 검색조건 콤보박스 생성
+            cboSearchCondition.Items.Add("전체");
+            cboSearchCondition.SelectedIndex = 0;
+
+            // 검색실행            
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            // SQLite 호출
+            // 없으면 DB 생성
+            // 또는 DB 파일 찾기
+            // 파일명은 GANAAN.db
+
         }
     }
 }
